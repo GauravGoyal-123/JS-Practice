c70d8c96ff68a8f616b8176d9c1e2f9f1d5cda0a
GauravGoyal-123 <78103586+GauravGoyal-123@users.noreply.github.com>
2023-12-25 08:20:34 +0000

    js practice

--- index.js
+++ index.js
@@ -6,7 +6,13 @@
 //     console.log(this);
 // }
 
-const chai = () => {
-    console.log(this);
-}
-chai();
+// arrow funciton (explicit return)
+// const chai = () => {
+//     console.log(this);
+// }
+
+// implicit return : it is good practice use simple bracket while returning anything which is required during returning an object in an implicit return. 
+// const Chai = () => ({name : "Gaurav Goyal"})
+
+// chai();
+
